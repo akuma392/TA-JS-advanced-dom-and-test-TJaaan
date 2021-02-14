7a9a5bfb522a852acacd4f4696446208dede7a25
Abhishek Kumar <abhivision3@gmail.com>
2021-02-14 13:02:32 +0530

    jest

---
+++ block-BJaajc/code/index.js
@@ -0,0 +1,52 @@
+function getFullName(firstName, lastName) {
+  return `${firstName} ${lastName}`;
+}
+
+function palindrome(number) {
+  let temp = number;
+  let final;
+
+  while (number > 0) {
+    rem = number % 10;
+    number = parseInt(number / 10);
+    final = final * 10 + rem;
+  }
+
+  if (number == final) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// const pi = 3.14;
+
+// function getCircumfrence(rad) {
+//   return `The circumference is ${2 * pi * rad}`;
+// }
+
+// function getArea(rad) {
+//   return `The area is ${pi * rad * rad}`;
+// }
+
+// function average(a, b) {
+//   return a + b / 2;
+// }
+
+// function longestWord(arr) {
+//   let longWord = '';
+
+//   arr.forEach((elm, i) => {
+//     if (elm.length > longWord.length) {
+//       longWord = elm;
+//     }
+//   });
+//   return longWord;
+// }
+
+// module.exports = getFullName;
+module.exports = palindrome;
+// module.exports = getCircumfrence;
+// module.exports = getArea;
+// module.exports = average;
+// module.exports = longestWord;
---
+++ block-BJaajc/code/index.test.js
@@ -0,0 +1,45 @@
+// const getFullName = require('./getFullName');
+
+// test('Abhi and kum  will be Abhi Kum', () => {
+//   expect(getFullName('Arya', 'Stark')).toBe('Arya Stark');
+// });
+
+const palindrome = require('./palindrome');
+
+test('12321 will be Palindrome', () => {
+  expect(palindrome(12321)).toBeTruthy();
+});
+
+// test('123321 will be Palindrome', () => {
+//   expect(palindrome(123321)).not.toBeTruthy();
+// });
+
+// const getCircumfrence = require('./getCircumfrence');
+
+// test('Circumference of 5 is 31.4', () => {
+//   expect(getCircumfrence(5)).toEqual(`The area is 31.4`);
+// });
+
+// // const getArea = require('./getArea');
+
+// const average = require('./average');
+
+// test('AVerage of 10 and 20 is 15', () => {
+//   expect(average(10, 20)).toBe(15);
+// });
+
+// test('AVerage of 30 and 20 is not 15', () => {
+//   expect(average(30, 20)).not.toBe(15);
+// });
+
+// test('AVerage of 20 and 20 is greater than 15', () => {
+//   expect(average(20, 20)).toBeGreaterThan(15);
+// });
+// test('AVerage of 30 and 20 is greater than 15', () => {
+//   expect(average(30, 20)).toBeLessThan(40);
+// });
+// test('AVerage of 10 and 10 is 10', () => {
+//   expect(average(10, 10)).toBe(10);
+// });
+
+// // const longestWord = require('./longestWord');
