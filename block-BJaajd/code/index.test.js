060f0af5f68b708403740c50aa5e4abac89d964b
Abhishek Kumar <abhivision3@gmail.com>
2021-02-17 11:06:53 +0530

    test for mock function

--- block-BJaajd/code/index.test.js
+++ block-BJaajd/code/index.test.js
@@ -7,3 +7,18 @@ test('should remove 3 and return [1,2,4]', () => {
 // test('should remove 3 and return [1,2,4]', () => {
 //   expect(methodsIndex.Cart.addToCart()).toContain([1, 2, 4]);
 // });
+
+test('should [1,2,3,4] return [2,4,6,8]', () => {
+  expect(methodsIndex.map([1, 2, 3, 4], (elm) => elm * 2)).toEqual([
+    2,
+    4,
+    6,
+    8,
+  ]);
+});
+
+test('should [1,2,3,4] return 10', () => {
+  expect(
+    methodsIndex.map([1, 2, 3, 4], (acc, cv, index) => (acc = acc + cv))
+  ).toEqual(10);
+});
